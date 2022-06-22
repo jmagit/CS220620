62effe3c7c4b0a6d5fa86156a4819086252f6c18
Javier <jmartin@grupoloyal.com>
2022-06-22 21:01:35 +0200

    Ejemplos

---
+++ DemosConsola/Program.cs
@@ -0,0 +1,42 @@
+﻿using MiBiblioteca;
+using System;
+
+
+namespace DemosConsola {
+#if DEBUG
+    public class Program {
+#else
+    class Program {
+#endif
+        static void Main(string[] args) {
+            char caracter = 'ﯔ';
+            caracter = (char)64;
+            caracter = '\n';
+            int i = 4;
+            object r = i; // new Int16(i)
+            i = (int)r;
+            long l = 444444;
+            short ss = (short)l;
+            
+            int? nulo = 4;
+            if (nulo.HasValue)
+                i = nulo.Value + 4;
+            string s = "c:\\dddd\\ddd\"";
+            String cad = @"c:\dddd\ddd";
+            string nombre = "Mundo";
+            string @string = "dd";
+#if DEBUG
+            Console.WriteLine("Hola " + nombre + ", que tal" + i + cad);
+#else
+            Console.WriteLine($"Hola {nombre}, que tal {1+i}");
+#endif
+            Calculadora calc = new Calculadora();
+            calc.Suma(4L, (long)3);
+
+            r = "";
+            i = int.Parse("222");
+            calc = (r as Calculadora);
+        }
+
+    }
+}
---
+++ MiBiblioteca/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MiBiblioteca {
+    public class Calculadora {
+        #region Atributos
+        #endregion
+        #region Métodos
+        /// <summary>
+        /// Método que suma dos valores
+        /// </summary>
+        /// <param name="a">Primer operando</param>
+        /// <param name="b">Segundo operando</param>
+        /// <returns>suma del primer operando con el segundo operando </returns>
+        public int Suma(int a, int b) { //a
+            return a + b;
+        }
+        public long Suma(long a, long b) { //b
+            return a + b;
+        }
+        #endregion
+    }
+}
